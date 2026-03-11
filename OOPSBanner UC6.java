577bd8ee611ac0adeca1330318663087b5db1f89
Shine1701 <nathshine2007@gmail.com>
2026-03-11 09:02:14 +0530

    OOPS Banner using Static Functions

---
+++ OOPSBanner UC6.java
@@ -0,0 +1,47 @@
+public class OOPSBanner {
+
+    // Static function for letter O
+    static String[] getO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Static function for letter P
+    static String[] getP() {
+        return new String[]{
+                "*****  ",
+                "*    * ",
+                "*****  ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Static function for letter S
+    static String[] getS() {
+        return new String[]{
+                " ***** ",
+                "*      ",
+                " ***** ",
+                "      *",
+                " ***** "
+        };
+    }
+
+    public static void main(String[] args) {
+
+        String[] O = getO();
+        String[] P = getP();
+        String[] S = getS();
+
+        // Loop to print banner
+        for (int i = 0; i < O.length; i++) {
+            System.out.println(O[i] + "  " + O[i] + "  " + P[i] + "  " + S[i]);
+        }
+    }
+}
